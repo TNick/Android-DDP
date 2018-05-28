ac7769801ca02df6455b1ea0c5a0c133af190c0f
Nicu Tofan <nicu.tofan@gmail.com>
2018-05-28 09:39:57 +0300

    merged changes from parall4x/Android-DDP

--- Source/library/src/main/java/im/delight/android/ddp/Meteor.java
+++ Source/library/src/main/java/im/delight/android/ddp/Meteor.java
@@ -27,6 +27,13 @@ import com.neovisionaries.ws.client.WebSocketFactory;
 import com.neovisionaries.ws.client.WebSocketFrame;
 import com.neovisionaries.ws.client.WebSocketListener;
 import com.neovisionaries.ws.client.WebSocketState;
+
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Queue;
@@ -39,6 +46,9 @@ import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.JsonNode;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 /** Client that connects to Meteor servers implementing the DDP protocol */
 public class Meteor {
@@ -71,6 +81,11 @@ public class Meteor {
 	private boolean mConnected;
 	private String mLoggedInUserId;
 	private final DataStore mDataStore;
+
+	// SSL related attributes
+	private SSLContext sslContext;
+
+	private String loginToken;
 
 	/**
 	 * Returns a new instance for a client connecting to a server via DDP over websocket
@@ -213,6 +228,63 @@ public class Meteor {
 		mReconnectAttempts = 0;
 	}
 
+	/**
+	 *
+	 * @param cert
+	 * @return
+	 */
+	public Meteor trustCaCert(Certificate cert) {
+
+		try {
+			// Create a KeyStore containing our trusted CAs
+			String keyStoreType = KeyStore.getDefaultType();
+			KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+			keyStore.load(null, null);
+			keyStore.setCertificateEntry("ca", cert);
+
+			// Create a TrustManager that trusts the CAs in our KeyStore
+			String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+			tmf.init(keyStore);
+
+			// Create an SSLContext that uses our TrustManager
+			SSLContext context = SSLContext.getInstance("TLS");
+			context.init(null, tmf.getTrustManagers(), null);
+
+			// SSL context ready for use
+			sslContext = context;
+
+
+		} catch (KeyStoreException keystoreException) {
+			log(TAG);
+			log(keystoreException.getMessage());
+			log(" provided CA cert will not be explicitly trusted");
+
+		} catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+			log(TAG);
+			log(noSuchAlgorithmException.getMessage());
+			log(" provided CA cert will not be explicitly trusted");
+
+		} catch (CertificateException certificateException) {
+			log(TAG);
+			log(certificateException.getMessage());
+			log(" provided CA cert will not be explicitly trusted");
+
+		} catch (IOException IOException) {
+			log(TAG);
+			log(IOException.getMessage());
+			log(" provided CA cert will not be explicitly trusted");
+
+		} catch (KeyManagementException keyManagementException) {
+			log(TAG);
+			log(keyManagementException.getMessage());
+			log(" provided CA cert will not be explicitly trusted");
+		}
+
+
+		return this;
+	}
+
 	/** Attempts to establish the connection to the server */
 	public void connect() {
 		openConnection(false);
@@ -247,7 +319,11 @@ public class Meteor {
 
 		// create a new WebSocket connection for the data transfer
 		try {
-			mWebSocket = new WebSocketFactory().setConnectionTimeout(30000).createSocket(mServerUri);
+			WebSocketFactory mWebSocketFactory = new WebSocketFactory();
+			// If an SSL context is available, add its socketfactory to the websocketfactory
+			if (sslContext != null) mWebSocketFactory.setSSLSocketFactory(sslContext.getSocketFactory());
+			mWebSocketFactory.setConnectionTimeout(30000);
+			mWebSocket = mWebSocketFactory.createSocket(mServerUri);
 		}
 		catch (final IOException e) {
 			mCallbackProxy.onException(e);
@@ -576,7 +652,7 @@ public class Meteor {
 						if (isLoginResult(resultData)) {
 							// extract the login token for subsequent automatic re-login
 							final String loginToken = resultData.get(Protocol.Field.TOKEN).getTextValue();
-							saveLoginToken(loginToken);
+							//setLoginToken(loginToken);
 
 							// extract the user's ID
 							mLoggedInUserId = resultData.get(Protocol.Field.ID).getTextValue();
@@ -913,7 +989,7 @@ public class Meteor {
 				mLoggedInUserId = null;
 
 				// delete the last login token which is now invalid
-				saveLoginToken(null);
+				setLoginToken(null);
 
 				if (listener != null) {
 					mCallbackProxy.forResultListener(listener).onSuccess(result);
@@ -1174,10 +1250,8 @@ public class Meteor {
 	 *
 	 * @param token the login token to save
 	 */
-	private void saveLoginToken(final String token) {
-		final SharedPreferences.Editor editor = getSharedPreferences().edit();
-		editor.putString(Preferences.Keys.LOGIN_TOKEN, token);
-		editor.apply();
+	public void setLoginToken(final String token) {
+		this.loginToken = token;
 	}
 
 	/**
@@ -1186,7 +1260,7 @@ public class Meteor {
 	 * @return the last login token or `null`
 	 */
 	private String getLoginToken() {
-		return getSharedPreferences().getString(Preferences.Keys.LOGIN_TOKEN, null);
+		return this.loginToken;
 	}
 
 	/**
@@ -1200,7 +1274,7 @@ public class Meteor {
 
 	private void initSession() {
 		// get the last login token
-		final String loginToken = getLoginToken();
+		//final String loginToken = getLoginToken();
 
 		// if we found a login token that might work
 		if (loginToken != null) {
@@ -1218,7 +1292,7 @@ public class Meteor {
 					mLoggedInUserId = null;
 
 					// discard the token which turned out to be invalid
-					saveLoginToken(null);
+					setLoginToken(null);
 
 					announceSessionReady(false);
 				}
